bfa6081d379c37787e3083a8f81732d3fa725256
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-11-27 19:02:23 +0200

    Removed try catch, replaced with promise

--- src/DatabaseLoader.ts
+++ src/DatabaseLoader.ts
@@ -3,69 +3,62 @@
 export class DatabaseLoader {
     private databaseURL: string = "http://localhost:3004";
     private databaseCountriesURL: string = "http://localhost:3004/countries";
-    public async GetCountries(): Promise<any> {
-        try {
-            const response = await axios.get(this.databaseCountriesURL);
-            return response.data;
-        } catch (error) {
-            console.error("Could not load countries from database:", error);
-            throw error;
-        }
+
+    public GetCountries(): Promise<any> {
+        return axios.get(this.databaseCountriesURL)
+            .then(response => response.data)
+            .catch(error => {
+                console.error("Could not load countries from database:", error);
+                throw error;
+            });
     }
 
-    private reverseSort:boolean = false;
-    private lastKeySorted:string;
+    private reverseSort: boolean = false;
+    private lastKeySorted: string;
 
-    public async GetCountries_Sorted(keyToSortBy: string) {
-        try {
+    public GetCountries_Sorted(keyToSortBy: string): Promise<any> {
+        this.reverseSort = this.lastKeySorted === keyToSortBy ? !this.reverseSort : false;
+        this.lastKeySorted = keyToSortBy;
+        let apiUrl: string;
 
-            this.reverseSort = this.lastKeySorted === keyToSortBy ? !this.reverseSort : false;
-
-            this.lastKeySorted = keyToSortBy;
-            let apiUrl:string;
-
-            if (this.inSearchMode) {
-                apiUrl = this.searchedApiUrl;
-                apiUrl += apiUrl.includes('?') ? '&' : '?';
-            } else {
-                apiUrl = this.databaseCountriesURL + '?';
-            }
-
-            apiUrl += this.reverseSort ? `_sort=${keyToSortBy}&_order=desc` : `_sort=${keyToSortBy}&_order=asc`;
-
-            const response = await axios.get(apiUrl);
-            return response.data;
-
-        } catch (error) {
-            console.error("Could not load countries from database", error);
-            throw error;
+        if (this.inSearchMode) {
+            apiUrl = this.searchedApiUrl;
+            apiUrl += apiUrl.includes('?') ? '&' : '?';
+        } else {
+            apiUrl = this.databaseCountriesURL + '?';
         }
+
+        apiUrl += this.reverseSort ? `_sort=${keyToSortBy}&_order=desc` : `_sort=${keyToSortBy}&_order=asc`;
+
+        return axios.get(apiUrl)
+            .then(response => response.data)
+            .catch(error => {
+                console.error("Could not load countries from database", error);
+                throw error;
+            });
     }
 
-    private searchedApiUrl:string;
-    public inSearchMode:boolean = false;
-    public async GetCountries_Search(name:string, capital:string, currency:string, language:string){
-        try {
+    private searchedApiUrl: string;
+    public inSearchMode: boolean = false;
 
-            let apiUrl:string = this.databaseCountriesURL + '?';
+    public GetCountries_Search(name: string, capital: string, currency: string, language: string): Promise<any> {
+        let apiUrl: string = this.databaseCountriesURL + '?';
 
-            if(name) apiUrl +=`&name=${name}`;
-            if(capital) apiUrl +=`&capital=${capital}`;
-            if(currency) apiUrl +=`&currency.name=${currency}`;
-            if(language) apiUrl +=`&language.name=${language}`;
+        if (name) apiUrl += `&name=${name}`;
+        if (capital) apiUrl += `&capital=${capital}`;
+        if (currency) apiUrl += `&currency.name=${currency}`;
+        if (language) apiUrl += `&language.name=${language}`;
 
-            this.searchedApiUrl = apiUrl;
+        this.searchedApiUrl = apiUrl;
 
-            const response = await axios.get(apiUrl);
-
-            this.inSearchMode = true;
-            return response.data;
-
-        } catch (error) {
-            console.error("Could not load countries from database", error);
-            throw error;
-        }
-
+        return axios.get(apiUrl)
+            .then(response => {
+                this.inSearchMode = true;
+                return response.data;
+            })
+            .catch(error => {
+                console.error("Could not load countries from database", error);
+                throw error;
+            });
     }
 }
-
